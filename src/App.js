74929b4ad5dfdd3cf4b4349f3af8b97de81e22e2
Mindy D Tran <mindytran98@gmail.com>
2024-02-05 15:23:30 -0500

    imported reducers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import { useReducer} from 'react';
 
 //importing data
 import {initialState} from "./data/toDo.js";
 
 //importing components
 import ToDoList from './components/ToDoList';
+import reducer from './reducers/toDoReducers.js';
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <h1>To Do App</h1>
-      <ToDoList initialState={initialState} />
+      <ToDoList state={todos} dispatch={dispatch} />
     </div>
   );
 }
