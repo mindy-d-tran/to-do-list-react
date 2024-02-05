fda36d39bd834605e9b4fde8d84f7e3a5226da96
Mindy D Tran <mindytran98@gmail.com>
2024-02-05 14:06:54 -0500

    importing data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+
+//importing data
+import {initialState} from "./data/toDo.js";
+
+//importing components
 import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="App">
       <h1>To Do App</h1>
-      <ToDoList/>
+      <ToDoList initialState={initialState} />
     </div>
   );
 }
