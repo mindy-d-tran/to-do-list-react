111351e37223621f5f36569e35888df683b6708d
Mindy D Tran <mindytran98@gmail.com>
2024-02-05 15:36:16 -0500

    useReduce to add new tasks

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,27 +1,43 @@
-// import { useState } from "react";
+import { useState } from "react";
 import ToDoItem from "./ToDoItem";
 
-function ToDoList({initialState}) {
-//   const [newTask, setNewTask] = useState("");
+function ToDoList({ state, dispatch }) {
+  const [title, setTitle] = useState("");
 
-//   const handleChange = (e) => {
-//     e.prevendDefault();
-//   };
+  // handle adding a new task to the data
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    dispatch({ type: "addTask", payload: { title } });
+    setTitle("");
+    console.log(title);
+  };
 
+  // updating task whenever there is a change in the text box
+  const handleOnChange = (e) => {
+    setTitle(e.target.value);
+    console.log(state);
+  };
   return (
     <div>
       <h2>to do list</h2>
 
       {/* form to add task to to-do list */}
-      <form>
-        <input type="text" placeholder="Add task" />
+      <form onSubmit={handleAddTask}>
+        <input
+          type="text"
+          placeholder="Add task"
+          onChange={handleOnChange}
+          value={title}
+        />
         <button>Add</button>
       </form>
 
       {/* form to check the todo list items */}
       <form>
-        {initialState.map((toDoItem) => {
-          return <ToDoItem key={toDoItem.id} item={toDoItem} />;
+        {state.map((toDoItem) => {
+          return (
+            <ToDoItem key={toDoItem.id} item={toDoItem} dispatch={dispatch} />
+          );
         })}
       </form>
     </div>
