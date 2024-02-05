37e673f29ca06725d806dfe251633968b57f8dd1
Mindy D Tran <mindytran98@gmail.com>
2024-02-05 13:19:08 -0500

    add ToDoList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="App">
       <h1>To Do App</h1>
+      <ToDoList/>
     </div>
   );
 }
