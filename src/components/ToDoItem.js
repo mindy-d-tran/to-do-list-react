88b6ce5b3843989452ae2ff5520c99ae8f543fb8
Mindy D Tran <mindytran98@gmail.com>
2024-02-06 10:03:34 -0500

    add everything to edit it

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 function ToDoItem(props) {
   const { item, dispatch } = props;
   const { id, title, completed } = item;
+
+  const [edit, setEdit] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
+
+  // updating task whenever there is a change in the text box
+  const handleOnChange = (e) => {
+    setNewTitle(e.target.value);
+    // console.log(state);
+  };
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
+
+  const handleEditItem = () => {
+    dispatch({ type: "handleEdit", payload: {} });
+    handleEdit();
+  };
 
   // handle deleting a task from the list
   const handleDeleteTask = () => {
@@ -10,16 +30,31 @@ function ToDoItem(props) {
 
   return (
     <div>
-      <input
-        type="checkbox"
-        id={id}
-        name={title}
-        value={title}
-        defaultChecked={completed}
-      />
-      <label>{title}</label>
-      <button id={id}>Edit</button>
-      <button onClick={handleDeleteTask}>Delete</button>
+      {edit ? (
+        <div onClick={handleEditItem}>
+          <input type="checkbox" id={id} name={title} value={title} />
+          <input
+            type="text"
+            placeholder="Add task"
+            onChange={handleOnChange}
+            value={newTitle}
+          />
+          <button>Save</button>
+        </div>
+      ) : (
+        <>
+          <input
+            type="checkbox"
+            id={id}
+            name={title}
+            value={title}
+            defaultChecked={completed}
+          />
+          <label>{title}</label>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDeleteTask}>Delete</button>
+        </>
+      )}
     </div>
   );
 }
