52ad11b0c3e1f5fcaae2e18cb0112bae249473f4
Mindy D Tran <mindytran98@gmail.com>
2024-02-06 09:48:15 -0500

    delete task is working no whitespace is allow?

--- src/reducers/toDoReducers.js
+++ src/reducers/toDoReducers.js
@@ -1,17 +1,18 @@
 export default function reducer(state, action) {
+  console.log(state);
   switch (action.type) {
     case "addTask":
       return [
         {
-            id: new Date().getTime(),
-            title: action.payload.title,
-            completed: false
+          id: new Date().getTime(),
+          title: action.payload.title,
+          completed: false,
         },
         ...state,
       ];
 
     case "deleteTask":
-      return "";
+      return state.filter((task) => task.id !== action.payload.id);
 
     case "editTask":
       return;
